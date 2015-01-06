cf19c63633bec3185e809e71ed4020f6dac2c52e
Ben Short <benshort.mouse@gmail.com>
2015-01-06 17:44:40 +0000

    Basic file structure started

---
+++ assets/partials/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="/assets/css/main.css">
+	<script type="text/javascript" src="/assets/js/modernizr.js"></script>
+	<title>Name</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,3 @@
+<? require_once 'assets/partials/head.php'; ?>
+<h1>Hello World!</h1>
+<? require_once 'assets/partials/footer.php';?>
