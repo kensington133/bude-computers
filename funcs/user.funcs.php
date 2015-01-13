5f0c6a6fd067ac77e97c1969a54cc72ffc594fb3
Ben Short <benshort.mouse@gmail.com>
2015-01-13 16:09:41 +0000

    userid is being saved in session, maybe isset() is to blame?

--- funcs/user.funcs.php
+++ funcs/user.funcs.php
@@ -4,7 +4,12 @@ function is_loggedin() {
 	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 		return true;
 	} else {
-		return isset($_SESSION['userid']);
+		// return isset($_SESSION['userid']);
+		if("" === $_SESSION['userid']){
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
 
